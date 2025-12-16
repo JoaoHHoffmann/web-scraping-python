5017cc0afd5500b7b06e24f51984e1d1a9f3506c
JoaoHoffmann <joaohhoffmann16@gmail.com>
2025-12-15 21:06:49 -0300

    Criando objeto dados página

--- webScraping.py
+++ webScraping.py
@@ -1,4 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Requisição HTTP para obter o conteúdo da página
 pagina = requests.get("https://quotes.toscrape.com/")
+
+dados_pagina = BeautifulSoup(pagina.text, 'html.parser')
+
+print(dados_pagina.prettify())
