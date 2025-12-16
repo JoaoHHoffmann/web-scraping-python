78d0d9ee90c1f3ffca1e90229a5ea77a63ce90eb
JoaoHoffmann <joaohhoffmann16@gmail.com>
2025-12-15 21:14:28 -0300

    Finalizando a raspagem trazendo todas as frases da página HTML

--- webScraping.py
+++ webScraping.py
@@ -6,4 +6,7 @@ pagina = requests.get("https://quotes.toscrape.com/")
 
 dados_pagina = BeautifulSoup(pagina.text, 'html.parser')
 
-print(dados_pagina.prettify())
+todas_frases = dados_pagina.find_all('span', itemprop='text')
+
+for div in todas_frases:
+    print(div.get_text())
